6a49b3abc8dbf0ba0a32e734b9b40055c1ab58d7
Nathaniel I Meyer <nathanielimeyer@gmail.com>
2017-03-03 10:24:46 -0800

    Add simple calculatePrice prototype

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,15 @@ function Pizza(size) {
   this.price = 0;
   this.toppings = [];
 }
+
+Pizza.prototype.calculatePrice = function() {
+  if (this.size === "S") {
+    this.price = 10;
+  } else if (this.size === "M") {
+    this.price = 14;
+  } else if (this.size === "L") {
+    this.price = 18;
+  } else if (this.size === "XL") {
+    this.price = 22;
+  }
+}
