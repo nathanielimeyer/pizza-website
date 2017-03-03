337999706769ec91ab32d48f880efd087c2f79ac
Nathaniel I Meyer <nathanielimeyer@gmail.com>
2017-03-03 10:40:30 -0800

    Add free toppings at various pizza sized; next will DRY out

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,24 @@ function Pizza(size) {
 
 Pizza.prototype.calculatePrice = function() {
   if (this.size === "S") {
-    this.price = 10 + this.toppings.length * 4;
+    this.price = 10;
+    if (this.toppings.length > 1) {
+      this.price += (this.toppings.length - 1) * 4;
+    }
   } else if (this.size === "M") {
-    this.price = 14 + this.toppings.length * 4;
+    this.price = 14;
+    if (this.toppings.length > 1) {
+      this.price += (this.toppings.length - 1) * 4;
+    }
   } else if (this.size === "L") {
-    this.price = 18 + this.toppings.length * 4;
+    this.price = 18;
+    if (this.toppings.length > 2) {
+      this.price += (this.toppings.length - 2) * 4;
+    }
   } else if (this.size === "XL") {
-    this.price = 22 + this.toppings.length * 4;
+    this.price = 22;
+    if (this.toppings.length > 3) {
+      this.price += (this.toppings.length - 3) * 4;
+    }
   }
 }
