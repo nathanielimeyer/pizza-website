db9f135deb48e02106a5f1b405205142b8f098e3
Nathaniel I Meyer <nathanielimeyer@gmail.com>
2017-03-03 10:30:22 -0800

    Add toppings to calculatePrice method

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,12 @@ function Pizza(size) {
 
 Pizza.prototype.calculatePrice = function() {
   if (this.size === "S") {
-    this.price = 10;
+    this.price = 10 + this.toppings.length * 4;
   } else if (this.size === "M") {
-    this.price = 14;
+    this.price = 14 + this.toppings.length * 4;
   } else if (this.size === "L") {
-    this.price = 18;
+    this.price = 18 + this.toppings.length * 4;
   } else if (this.size === "XL") {
-    this.price = 22;
+    this.price = 22 + this.toppings.length * 4;
   }
 }
